736d70fe2de777b67ba7d67b0acc87d2f6d733a1
shash-hq <shashankranjan.yt@gmail.com>
2026-03-20 05:10:34 +0530

    fix: disable email verification for production demo

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -59,7 +59,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function bookmarkedVideos()
     {
-        return $this->belongsToMany(Video::class, 'bookmarks', 'user_id', 'video_id');
+        return $this->belongsToMany(Video::class , 'bookmarks', 'user_id', 'video_id');
     }
 
     public function isAdmin()
